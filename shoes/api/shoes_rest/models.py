e4c59762469edfb25547a6459dcd03d0f69a08e0
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-17 15:43:52 -0400

    Created and fixed model and VO, re-made migrations, added some bins

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class BinVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+
+    closet_name = models.CharField(max_length=100)
+    bin_number = models.PositiveSmallIntegerField()
+    bin_size = models.PositiveSmallIntegerField()
 
 
 class Shoe(models.Model):
@@ -8,3 +16,8 @@ class Shoe(models.Model):
     size = models.PositiveSmallIntegerField(primary_key=True)
     colour = models.CharField(max_length=150)
     style = models.CharField(max_length=150)
+    bin = models.ForeignKey(
+        BinVO,
+        related_name="shoes",
+        on_delete=models.CASCADE,
+    )

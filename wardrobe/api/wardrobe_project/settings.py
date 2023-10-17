a7456c698f4505864aef96e28708de75e217d99b
Seth Porche <porchest2022@outlook.com>
2023-10-17 14:03:39 -0400

    installed hats app

--- wardrobe/api/wardrobe_project/settings.py
+++ wardrobe/api/wardrobe_project/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hats.apps.HatsConfig',
+    'shoes.apps.ShoesConfig',
+
 ]
 
 MIDDLEWARE = [
